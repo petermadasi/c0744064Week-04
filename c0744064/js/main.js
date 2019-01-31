2a93f44c986e4799a5cf94ceef8949da747023cc
visualstudio <visualstudio@230-PC14>
2019-01-31 11:45:19 -0500

    Message Display using function

--- c0744064/js/main.js
+++ c0744064/js/main.js
@@ -1,2 +1,3 @@
 ﻿// Your code here!
-document.write("HI");
+var date = new Date();
+document.body.innerHTML = "<h1>Date today is " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>";
